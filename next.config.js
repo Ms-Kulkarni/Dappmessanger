be3760fa5b1677d03818d9fd224889a32f614a69
Ms kulkarni <mskulkarnik2@gmail.com>
2022-10-20 22:57:39 +0530

    dapp massenger login

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// /** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains:['avatars.dicebar.com', 'links.papareact.com'],
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Login from '../components/login'
+import {useMoralis} from 'react-moralis'
+
+export default function Home() {
+
+ 
+  const {isAuthenticated, logout} = useMoralis();
+
+  if(!isAuthenticated) return <Login/>
+
+
+
+  return (
+    <div className='h-screen'>
+      <Head>
+        <title>Dapp Messanger</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Wellcome to App</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+
+    
+  );
+}
+
